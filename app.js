44ae890f60c7f284dc91c9ed469a4bc9e93922cd
Scott Burbidge <scottisreally@gmail.com>
2014-11-02 15:43:25 -0800

    finished static express app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var http = require('http');
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 8080)
